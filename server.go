310d8cf3c550d04e9b0023324b4b07985e12d214
Maxime Heckel <heckelmaxime@gmail.com>
2015-01-23 19:43:35 +0100

    factorise code

--- server.go
+++ server.go
@@ -22,6 +22,25 @@ func init() {
 	flag.StringVar(&imageFile, "i", "", "Image File")
 }
 
+func readFile(file string) ([]byte, string) {
+	imgfile, err := os.Open(file)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer imgfile.Close()
+
+	fileInfo, _ := imgfile.Stat()
+	filename := fileInfo.Name()
+	size := fileInfo.Size()
+	bytes := make([]byte, size)
+
+	buffer := bufio.NewReader(imgfile)
+	_, err = buffer.Read(bytes)
+
+	return bytes, filename
+}
+
 func main() {
 	flag.Parse()
 
@@ -30,8 +49,10 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
+
+	bytes, filename := readFile(imageFile)
 	//IMAGE OPEN
-	fImg1, err := os.Open(imageFile)
+	/*fImg1, err := os.Open(imageFile)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -44,7 +65,7 @@ func main() {
 
 	//READ BUFFER
 	buffer := bufio.NewReader(fImg1)
-	_, err = buffer.Read(bytes)
+	_, err = buffer.Read(bytes)*/
 
 	//Set new S3 connection
 	s := s3.New(auth, aws.USEast)
@@ -52,9 +73,9 @@ func main() {
 	//Set connection to bucket
 	bucket := s.Bucket(bucketName)
 
-	//Type of file
+	//File Info
 	filetype := http.DetectContentType(bytes)
-	err = bucket.Put("sample.jpeg", bytes, filetype, s3.BucketOwnerFull)
+	err = bucket.Put(filename, bytes, filetype, s3.BucketOwnerFull)
 	if err != nil {
 		panic(err.Error())
 	}
