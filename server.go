79984976231407b22da4133496dc62af07a31d43
Maxime Heckel <heckelmaxime@gmail.com>
2015-01-23 19:44:01 +0100

    wash code

--- server.go
+++ server.go
@@ -51,22 +51,6 @@ func main() {
 	}
 
 	bytes, filename := readFile(imageFile)
-	//IMAGE OPEN
-	/*fImg1, err := os.Open(imageFile)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer fImg1.Close()
-
-	fileInfo, _ := fImg1.Stat()
-	var size = fileInfo.Size()
-	bytes := make([]byte, size)
-
-	//READ BUFFER
-	buffer := bufio.NewReader(fImg1)
-	_, err = buffer.Read(bytes)*/
-
 	//Set new S3 connection
 	s := s3.New(auth, aws.USEast)
 
